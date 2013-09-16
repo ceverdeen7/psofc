17d907e3be6fc8a7d08e58f771e4f1aae134a262
alvindaiyan <alvindaiyan@gmail.com>
2013-09-16 09:19:42 +0000

    done the script for grid and latex output

---
+++  psofc/PSOfc2.2/shs_array_mulfc/wine_array_mulfc_DT_new_script.sh
@@ -0,0 +1,32 @@
+#$ -S /bin/sh 
+#$ -wd /vol/grid-solar/sgeusers/daiyan
+##$ -M Yan.Dai@ecs.vuw.ac.nz
+##$ -m be 
+
+mkdir -p /local/tmp/daiyan/$JOB_ID 
+
+if [ -d /local/tmp/daiyan/$JOB_ID ]; then
+        cd /local/tmp/daiyan/$JOB_ID
+else
+        echo "There's no job directory to change into" 
+        echo "Here's LOCAL TMP "
+        ls -la /local/tmp
+        echo "AND LOCAL TMP FRED "
+        ls -la /local/tmp/daiyan
+        echo "Exiting"
+        exit 1
+
+fi
+
+cp /vol/grid-solar/sgeusers/daiyan/run_array_mul_sep_16/array_mulfcjava.jar .
+mkdir file_array_mulfc
+cp -r /vol/grid-solar/sgeusers/daiyan/Data .
+java -jar array_mulfcjava.jar wine
+
+rm array_mulfcjava.jar
+cp -r *  /vol/grid-solar/sgeusers/daiyan/run_array_mul_sep_16/
+rm -r Data
+rm -r file_array_mulfc
+cd /vol/grid-solar/sgeusers/daiyan
+pwd
+rm -fr /local/tmp/daiyan/$JOB_ID
